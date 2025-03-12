aa9051de533b771ee546e0226d08a072eba030dc
dhwpdnr <ink1237@gmail.com>
2025-03-12 16:22:10 +0900

    chore: elk docker compose update
    add health checker, seperate password to .env

--- app/config/settings.py
+++ app/config/settings.py
@@ -23,10 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r0p)66xwi^3&idnfio@5e5&ts_j2$g@qv6l5_iiz+p_8m_i463'
-
-# SECURITY WARNING: don't run with debug turned on in production!
+SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = []
